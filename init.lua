c4a628ca04ec539490382e988a49274d5df7a07b
LoneWolfHT <lonewolf04361@gmail.com>
2022-05-09 15:36:58 -0700

    Improve Code

--- init.lua
+++ init.lua
@@ -3,28 +3,31 @@ local gamefile = worldpath.."/api/game.json"
 
 if table.indexof(minetest.get_dir_list(worldpath, true), "api") == -1 then
 	minetest.log("[server_stats] /api/ folder not found in world dir, aborting...")
-	return
+
+	return -- Comment out when testing
+
+	minetest.mkdir(worldpath.."/api/")
 end
 
-local DATA = {}
-local function update()
+local function update(data)
 	local file = io.open(gamefile, "w")
 
-	file:write(minetest.write_json(DATA, true))
+	file:write(minetest.write_json(data, true))
 
 	file:close()
 end
 
-ctf_api.register_on_mode_start(function()
-	DATA.current_mode = {
-		name = ctf_modebase.current_mode,
-		matches = ctf_modebase.current_mode_matches,
-	}
-end)
-
 ctf_api.register_on_new_match(function()
-	DATA.current_map = {name = ctf_map.current_map.name, technical_name = ctf_map.current_map.dirname}
-	DATA.current_mode.matches_played = ctf_modebase.current_mode_matches_played
-
-	update()
+	update({
+		current_mode = {
+			name = ctf_modebase.current_mode,
+			matches = ctf_modebase.current_mode_matches,
+			matches_played = ctf_modebase.current_mode_matches_played,
+		},
+		current_map = {
+			name = ctf_map.current_map.name,
+			technical_name = ctf_map.current_map.dirname,
+			start_time = os.time(), -- Can be converted to local date here https://www.unixtimestamp.com/
+		},
+	})
 end)
